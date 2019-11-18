fa3b9d14f04e45917b6d0f287ba95d9b020f5d0a
Laravel Shift <shift@laravelshift.com>
2019-11-18 16:51:53 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them so you can review the commit diff for
    changes. Moving forward, you should use ENV variables or create a
    separate config file to allow the core config files to remain
    automatically upgradeable.

--- config/services.php
+++ config/services.php
@@ -8,25 +8,26 @@ return [
     |--------------------------------------------------------------------------
     |
     | This file is for storing the credentials for third party services such
-    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
-    | default location for this type of information, allowing packages
-    | to have a conventional place to find your various credentials.
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
     |
     */
 
-    'facebook' => [
-        'app_id' => env('FACEBOOK_APP_ID'),
-        'admins' => env('FACEBOOK_ADMINS'),
-        'page_url' => env('FACEBOOK_PAGE_URL'),
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
-    'googleanalytics' => [
-        'id' => env('GOOGLEANALYTICS_ID'),
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
     ],
 
-    'twitter' => [
-        'site' => env('TWITTER_SITE'),
-        'creator' => env('TWITTER_CREATOR'),
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
 ];
