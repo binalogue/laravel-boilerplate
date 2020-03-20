e82e207df19a2c2e65c5dbd969fae568c59cd9b8
Mikel Goig <m@mikelgoig.com>
2020-03-20 13:11:59 +0000

    🛸 Update Nova footer

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,15 @@
-<p class="mt-8 text-center text-xs text-80">
-    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+<p class="flex items-center justify-center mt-8 text-center text-xs text-80">
+    &copy; {{ date('Y') }} <a href="https://binalogue.com" class="ml-1 text-primary no-underline">Binalogue</a>
+
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
+
+    @include('vendor.nova.partials.footer_logo')
+
     <span class="px-1">&middot;</span>
+
+    Made with <a href="https://nova.laravel.com" class="ml-1 text-primary no-underline">Laravel Nova</a>
+
+    <span class="px-1">&middot;</span>
+
     v{{ \Laravel\Nova\Nova::version() }}
 </p>
