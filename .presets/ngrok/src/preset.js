dccbbc60a89ea4a6259de623418f3a2f8cd82726
Mikel Goig <m@mikelgoig.com>
2020-09-15 14:35:21 +0100

    🏗 Add ngrok preset

---
+++ .presets/ngrok/src/preset.js
@@ -0,0 +1,35 @@
+const { Preset } = require('use-preset');
+
+module.exports = Preset.make('laravel-boilerplate-ngrok-preset')
+
+  .prompts()
+  .input('Your ngrok auth token', 'ngrok_auth_token')
+  .chain()
+
+  .edit('docker-compose.yml')
+  .title('🐳 Add ngrok job')
+  .search(/MYSQL_PASSWORD: root/)
+  .addAfter(
+    `
+  ngrok:
+    container_name: laravel-boilerplate-ngrok
+    image: wernight/ngrok
+    depends_on:
+      - app
+    networks:
+      - app-tier
+    ports:
+      - 4040:4040
+    environment:
+      NGROK_AUTH: {NGROK_AUTH_TOKEN}
+      NGROK_PORT: host.docker.internal:80
+      NGROK_REGION: eu`
+  )
+  .end()
+  .chain()
+
+  .edit('docker-compose.yml')
+  .title('🔒 Add ngrok auth token')
+  .replace('{NGROK_AUTH_TOKEN}')
+  .with(context => context.prompts.ngrok_auth_token)
+  .chain();
