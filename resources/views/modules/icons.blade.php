07fbdcc8ef883f69b274b070c88f94555a0a958a
Mikel Goig <m@mikelgoig.com>
2020-09-30 19:21:31 +0100

    🍱 Update favicon background color

--- resources/views/modules/icons.blade.php
+++ resources/views/modules/icons.blade.php
@@ -3,7 +3,7 @@
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1db954">
-<meta name="msapplication-TileColor" content="#1db954">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff">
+<meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
-<meta name="theme-color" content="#1db954">
+<meta name="theme-color" content="#ffffff">
